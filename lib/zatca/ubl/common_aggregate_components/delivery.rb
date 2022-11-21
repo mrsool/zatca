8602ce3d4817e1dd7552189b6ef4bee267c58a96
Omar Bahareth <omar@omar.engineer>
2022-11-22 02:32:32 +0300

    Allow Nullable Latest Delivery Date on Delivery

--- lib/zatca/ubl/common_aggregate_components/delivery.rb
+++ lib/zatca/ubl/common_aggregate_components/delivery.rb
@@ -1,7 +1,7 @@
 class ZATCA::UBL::CommonAggregateComponents::Delivery < ZATCA::UBL::BaseComponent
   attr_reader :scheme_id, :id
 
-  def initialize(latest_delivery_date:, actual_delivery_date:)
+  def initialize(actual_delivery_date:, latest_delivery_date: nil)
     super()
 
     @latest_delivery_date = latest_delivery_date
@@ -12,10 +12,16 @@ class ZATCA::UBL::CommonAggregateComponents::Delivery < ZATCA::UBL::BaseComponen
     "cac:Delivery"
   end
 
+  def latest_delivery_date_element
+    return nil if @latest_delivery_date.nil?
+
+    ZATCA::UBL::BaseComponent.new(name: "cbc:LatestDeliveryDate", value: @latest_delivery_date)
+  end
+
   def elements
     [
       ZATCA::UBL::BaseComponent.new(name: "cbc:ActualDeliveryDate", value: @actual_delivery_date),
-      ZATCA::UBL::BaseComponent.new(name: "cbc:LatestDeliveryDate", value: @latest_delivery_date)
+      latest_delivery_date_element
     ]
   end
 end
