8502c0cfabd899a53d8ccd8ab0dd5f7ab07d17df
Omar Bahareth <omar@omar.engineer>
2023-03-15 14:29:57 +0300

    Fix TLV Generation to Match Latest ZATCA Specs
    ZATCA previously instructed to convert strings to 8-bit ASCII but now they allow UTF8 characters and throw errors if you return 8-bit ASCII.

--- lib/zatca/tag.rb
+++ lib/zatca/tag.rb
@@ -28,11 +28,16 @@ module ZATCA
       # TLV should be concatenated together without any separator in the following
       # format: character_value_of_id character_value_of_value_length value_itself
       # All of this should be in 8-bit ASCII.
-      tlv = @id.chr + @value.bytesize.chr + value
+      # tlv = @id.chr + @value.bytesize.chr + value
 
+      id_as_hex = [sprintf("%02X", @id)].pack("H*")
+      value_length_as_hex = [sprintf("%02X", @value.bytesize)].pack("H*")
+
+      tlv = [id_as_hex, value_length_as_hex, value].join
+      # Base64.strict_encode64(tlv)
       # We need to use force_encoding because encode will raise errors when
       # trying to encode a string with utf-8 characters.
-      tlv.force_encoding("ASCII-8BIT")
+      # tlv.force_encoding("ASCII-8BIT")
     end
   end
 end
--- spec/lib/zatca/tags_spec.rb
+++ spec/lib/zatca/tags_spec.rb
@@ -4,11 +4,17 @@ describe ZATCA::Tags do
 
   describe "#to_base64" do
     it "generates valid Base64 with ASCII tags" do
-      expect(tags.to_base64).to eq("AQZNcnNvb2wCDzMxMDIyODgzMzQwMDAwMwMZMjAyMS0xMC0yMFQxOToyOTozMiswMzowMAQDMTE1BQIxNQ==")
+      base64 = tags.to_base64
+      expected_base64 = "AQZNcnNvb2wCDzMxMDIyODgzMzQwMDAwMwMZMjAyMS0xMC0yMFQxOToyOTozMiswMzowMAQDMTE1BQIxNQYsSU1ybEhPMWdicWJqc3g2akMwMWxiNjc3T1A1WHd5akluWFNxbldINTViaz0HYE1FVUNJQy9RVlA1S3pYbUEwRVYwMk9SYTMySkp5UDc4dGRzVXpZSThrS29PQm55QUFpRUEybk5RWktOZ25VcnVha1NYbVY0TStNc3RYOTF6bGtnNTFpVE1XbTB3UWNNPQhYMFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEYYMMoOaFYAhMO/steotfZyavr6p11SSlwsK9azmsLY7b1b+FLhqMArhB2dqHKboxqKNfvkKDePhpqjui5hcn0QlIMEYCIQDuYdPrKDzmO1AZanczu09PsmTb7Oy9UcazdtTlntgTrwIhAPrR5tBqZiNi915ucWM1/Hhfh2insuwQEUI1KwtjQgVp"
+
+      expect(base64).to eq(expected_base64)
     end
 
     it "generates valid Base64 with unicode tags" do
-      expect(unicode_tags.to_base64).to eq("AQrZhdix2LPZiNmEAg8zMTAyMjg4MzM0MDAwMDMDGTIwMjEtMTAtMjBUMTk6Mjk6MzIrMDM6MDAEAzExNQUCMTU=")
+      base64 = unicode_tags.to_base64
+      expected_base64 = "AQrZhdix2LPZiNmEAg8zMTAyMjg4MzM0MDAwMDMDGTIwMjEtMTAtMjBUMTk6Mjk6MzIrMDM6MDAEAzExNQUCMTUGLElNcmxITzFnYnFianN4NmpDMDFsYjY3N09QNVh3eWpJblhTcW5XSDU1Yms9B2BNRVVDSUMvUVZQNUt6WG1BMEVWMDJPUmEzMkpKeVA3OHRkc1V6WUk4a0tvT0JueUFBaUVBMm5OUVpLTmduVXJ1YWtTWG1WNE0rTXN0WDkxemxrZzUxaVRNV20wd1FjTT0IWDBWMBAGByqGSM49AgEGBSuBBAAKA0IABGGDDKDmhWAITDv7LXqLX2cmr6+qddUkpcLCvWs5rC2O29W/hS4ajAK4Qdnahym6MaijX75Cg3j4aao7ouYXJ9EJSDBGAiEA7mHT6yg85jtQGWp3M7tPT7Jk2+zsvVHGs3bU5Z7YE68CIQD60ebQamYjYvdebnFjNfx4X4dop7LsEBFCNSsLY0IFaQ=="
+
+      expect(base64).to eq(expected_base64)
     end
   end
 end
