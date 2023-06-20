efbc1863b222ff2a35ed01cb1a78076077ec73ed
Omar Bahareth <omar@omar.engineer>
2023-06-20 18:32:56 +0300

    Extract Certificate Public Key and Certificate as Bytes
    Extract r and s as specified in ZATCA’s docs.

--- lib/zatca/signing/certificate.rb
+++ lib/zatca/signing/certificate.rb
@@ -1,7 +1,7 @@
 class ZATCA::Signing::Certificate
   attr_accessor :serial_number, :issuer_name, :cert_content_without_headers,
     :hash, :public_key, :public_key_without_headers, :signature,
-    :public_key_bytes, :unpacked_signature
+    :public_key_bytes, :unpacked_signature, :signature_bytes
 
   # Returns the certificate hashed with SHA256 then Base64 encoded
   def self.generate_base64_hash(base64_certificate)
@@ -58,9 +58,29 @@ class ZATCA::Signing::Certificate
       .gsub("-----BEGIN PUBLIC KEY-----", "")
       .gsub("-----END PUBLIC KEY-----", "")
       .delete("\n")
-    @public_key_bytes = Base64.strict_decode64(@public_key_without_headers)
+
+    @public_key_bytes = parse_public_key_bytes
 
     parse_signature
+  end
+
+  def parse_public_key_bytes
+    # We call public_key twice to get the EC Point from the certificate
+    public_key = openssl_certificate.public_key.public_key
+
+    raise "Public key is not an EC Point." unless public_key.is_a?(OpenSSL::PKey::EC::Point)
+
+    bn = OpenSSL::BN.new(public_key.to_bn.to_s(2)[1..-1], 2) # skip first byte (04)
+    ecdsa_public_key = [bn.to_s(16)].pack("H*")
+
+    # Ensure public_key is 64 bytes long
+    if ecdsa_public_key.bytesize < 64
+      "\x00" * (64 - ecdsa_public_key.bytesize) + ecdsa_public_key
+    elsif ecdsa_public_key.bytesize > 64
+      ecdsa_public_key[-64..-1]
+    else
+      ecdsa_public_key
+    end
   end
 
   def parse_signature
@@ -76,5 +96,12 @@ class ZATCA::Signing::Certificate
     # So it ends up looking like so:
     # "30:46:02:21:00:ee:61:d3:eb:28:3c:e6:3b:50:19:6a:77:33:bb:4f:4f:b2:64:db:ec:ec:bd:51:c6:b3:76:d4:e5:9e:d8:13:af:02:21:00:fa:d1:e6:d0:6a:66:23:62:f7:5e:6e:71:63:35:fc:78:5f:87:68:a7:b2:ec:10:11:42:35:2b:0b:63:42:05:69"
     @unpacked_signature = @signature.unpack1("H*").gsub(/(..)/, '\1:')[0..-2]
+
+    signature_asn1 = OpenSSL::ASN1.decode(asn1.value.last.value)
+    # Extract raw bytes from the signature (for QR Code)
+    r, s = signature_asn1.value.map(&:value)
+
+    # Convert r and s to their binary representation and concatenate them to get the raw signature
+    @signature_bytes = [r.to_s(2).rjust(32, "\x00"), s.to_s(2).rjust(32, "\x00")].join
   end
 end
